01d257f8e4187674937daa913c659d1bcd9915ff
Matheus N <Matheus N>
2019-02-18 21:25:32 -0300

    revisão docs

---
+++ application/views/tela_principal.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>BitBloco - Painel Principal </title>
+<link href="../../style/estilo.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+<div id="general">
+	<div id="top">
+    	<div id="logo">
+        <img src="../../image/icone.png" height="100px" width="100px"/>
+        </div>
+        <div id="slogam">
+        <h3 align="center"> Aqui vai um texto animado e/ou slogam </h1>        	
+        </div>
+    </div>
+	<div id="menu">
+    <!-- <h3 align="justify"> <font color="#FFFFFF"> Aqui vai o Menu </font> </h3> -->
+    
+    </div>
+    <div id="ranking">
+    <h1 align="center"> Aqui vai o ranking </h1>
+    </div>
+</div>
+</body>
+</html>
