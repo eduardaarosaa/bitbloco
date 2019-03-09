7e98ea8f92f49fcf2961504425c02186fd241b84
FelipeJulianoDev <225872017@eniac.edu.br>
2019-03-09 16:16:48 -0300

    tela meu perfil

--- application/views/tela_perfil.php
+++ application/views/tela_perfil.php
@@ -6,3 +6,117 @@
         <div class="row">
             <div class="col-12 divEspaço"></div>
         </div>
+
+        <style type="text/css">
+	#perfilimg {
+		position:absolute;
+		left:30%;
+		top:15%;
+		margin-left:-110px;
+		margin-top:-40px;
+		}
+</style>
+<img src="../../image/perfilimg.png" alt=”some text” width=150 height=150 id="perfilimg"/>
+
+<style type="text/css">
+	#textosperfil0 {
+		position:absolute;
+		left:50%;
+		top:15%;
+		margin-left:-110px;
+		margin-top:-40px;
+		}
+</style>
+
+<div class="informacoes0" id="textosperfil0">
+
+	<div class="informacoes-2">
+
+<h2>Minhas Informações</h2>
+
+    </div>
+
+    </div>   
+
+
+<style type="text/css">
+	#textosperfil {
+		position:absolute;
+		left:50%;
+		top:25%;
+		margin-left:-110px;
+		margin-top:-40px;
+		}
+</style>
+
+<div class="informacoes" id="textosperfil">
+
+	<div class="informacoes-2">
+
+<h4>Nome</h4>
+
+    </div>
+    
+    <div class="informacoes-3">
+
+<h4>Sobrenome</h4>
+
+    </div>
+    
+    <div class="informacoes-4">
+
+<h4>Idade</h4>
+
+    </div>
+    
+    <div class="informacoes-5">
+
+<h4>Carteira</h4>
+
+	</div>
+    
+ 
+
+</div>
+
+<style type="text/css">
+	#textosperfil2 {
+		position:absolute;
+		left:60%;
+		top:25%;
+		margin-left:-110px;
+		margin-top:-40px;
+		}
+</style>
+
+<div class="informacoesv2" id="textosperfil2">
+
+	<div class="informacoes-6">
+
+<p>XXXXXXXXXXX</p>
+
+    </div>
+    
+    <div class="informacoes-7">
+
+<p>XXXXXXXXXXX</p>
+
+    </div>
+    
+    <div class="informacoes-8">
+
+<p>XXXXXXXXXXX</p>
+
+    </div>
+    
+    <div class="informacoes-9">
+
+<p>XXXXXXXXXXX</p>
+
+	</div>
+    
+ 
+
+</div>
+       
+        
