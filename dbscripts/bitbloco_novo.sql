3e542cfa87fa0bc55f26f91f978ce3630dae7e79
Duda <eduardacirina@gmail.com>
2019-04-02 23:39:25 -0300

    banco de dados novo

---
+++ dbscripts/bitbloco_novo.sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 03-Abr-2019 às 04:32
+-- Versão do servidor: 10.1.37-MariaDB
+-- versão do PHP: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bitbloco`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `carteira`
+--
+
+CREATE TABLE `carteira` (
+  `id` int(11) NOT NULL,
+  `num_carteira` int(11) NOT NULL,
+  `quantidade` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `carteira`
+--
+
+INSERT INTO `carteira` (`id`, `num_carteira`, `quantidade`) VALUES
+(1, 1, '1'),
+(2, 291291821, '3');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `questoes`
+--
+
+CREATE TABLE `questoes` (
+  `cod` int(11) NOT NULL,
+  `questao1` varchar(300) NOT NULL,
+  `questao2` varchar(300) NOT NULL,
+  `questao3` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `questoes`
+--
+
+INSERT INTO `questoes` (`cod`, `questao1`, `questao2`, `questao3`) VALUES
+(1, 'Some 1+2', 'some 2+2', 'some 2+3');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(20) NOT NULL,
+  `nome` varchar(40) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `telefone` varchar(40) DEFAULT NULL,
+  `senha` varchar(40) DEFAULT NULL,
+  `num_carteira` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nome`, `email`, `telefone`, `senha`, `num_carteira`) VALUES
+(1, 'Eduarda', 'eduardacirina@gmail.com', '1111211212', '123', NULL),
+(2, 'Milena', 'milena@gmail.com', '271717171721', '123', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `carteira`
+--
+ALTER TABLE `carteira`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `questoes`
+--
+ALTER TABLE `questoes`
+  ADD PRIMARY KEY (`cod`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `carteira`
+--
+ALTER TABLE `carteira`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `questoes`
+--
+ALTER TABLE `questoes`
+  MODIFY `cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `num_carteira` FOREIGN KEY (`id`) REFERENCES `carteira` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
