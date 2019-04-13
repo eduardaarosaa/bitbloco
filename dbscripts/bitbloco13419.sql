884d574eb6f01043d965385047dedfa2df8a22fd
Duda <eduardacirina@gmail.com>
2019-04-13 13:54:19 -0300

    banco novo

--- dbscripts/bitbloco13419.sql
+++ dbscripts/bitbloco13419.sql
@@ -1,4 +1,13 @@
-﻿
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 13-Abr-2019 às 18:49
+-- Versão do servidor: 10.1.37-MariaDB
+-- versão do PHP: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -9,12 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-create database bitbloco;
-
-use bitbloco;
 --
 -- Database: `bitbloco`
-
 --
 
 -- --------------------------------------------------------
@@ -25,7 +30,6 @@ use bitbloco;
 
 CREATE TABLE `carteira` (
   `id` int(11) NOT NULL,
-  `num_carteira` int(11) NOT NULL,
   `quantidade` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -33,9 +37,9 @@ CREATE TABLE `carteira` (
 -- Extraindo dados da tabela `carteira`
 --
 
-INSERT INTO `carteira` (`id`, `num_carteira`, `quantidade`) VALUES
-(1, 1, '1'),
-(2, 291291821, '3');
+INSERT INTO `carteira` (`id`, `quantidade`) VALUES
+(1, '1'),
+(2, '3');
 
 -- --------------------------------------------------------
 
@@ -77,8 +81,8 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nome`, `email`, `telefone`, `senha`, `num_carteira`) VALUES
-(1, 'Eduarda', 'eduardacirina@gmail.com', '1111211212', '123', NULL),
-(2, 'Milena', 'milena@gmail.com', '271717171721', '123', NULL);
+(1, 'Eduarda', 'eduardacirina@gmail.com', '1111211212', '123', 1),
+(2, 'Milena', 'milena@gmail.com', '271717171721', '123', 2);
 
 --
 -- Indexes for dumped tables
